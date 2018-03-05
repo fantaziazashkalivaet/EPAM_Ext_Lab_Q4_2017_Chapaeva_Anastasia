d4d1a0e6eab1961980899677a61e54e2b08f4ca5
fantaziazashkalivaet <fantaziazashkalivaet@gmail.com>
2018-03-05 11:51:59 +0400

    .

--- HWT_10/Procedure.sql
+++ HWT_10/Procedure.sql
@@ -17,7 +17,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 
-/*
+/*13.1
 Написать процедуру, которая возвращает самый крупный заказ 
 для каждого из продавцов за определенный год. 
 В результатах не может быть несколько заказов одного продавца, 
@@ -31,15 +31,90 @@ GO
 Процедура должна быть реализована с использованием оператора SELECT и БЕЗ ИСПОЛЬЗОВАНИЯ КУРСОРОВ.
 */
 GO
-CREATE PROCEDURE GreatestOrders (@Year int, @Quantity int)
+CREATE OR ALTER PROCEDURE GreatestOrders (@Year int, @Quantity int)
 AS
 BEGIN
-SELECT CONCAT(E.FirstName, ' ', E.LastName), O.OrderID, (OD.UnitPrice * OD.Quantity * (1 - OD.Discount)) as [Amount]
-FROM Employees as E JOIN Orders as O 
-ON E.EmployeeID = O.EmployeeID JOIN [Order Details] as OD
-ON O.OrderID = OD.OrderID
-WHERE YEAR(O.OrderDate) = @Year
+SELECT TOP(@Quantity) [Name], MAX([Amount]) as [MaxAmount]
+FROM 
+	(SELECT E.FirstName as [Name], 
+		O.OrderID as [ID], 
+		OD.UnitPrice*OD.Quantity*(1 - OD.Discount) as [Amount]
+	FROM Employees as E JOIN Orders as O 
+	ON E.EmployeeID = O.EmployeeID JOIN [Order Details] as OD
+	ON O.OrderID = OD.OrderID
+	WHERE YEAR(O.OrderDate) = @Year) as Tab
+GROUP BY [Name];
 END;
 GO
 
 
+/*13.2
+Написать процедуру, которая возвращает заказы в таблице Orders, 
+согласно указанному сроку доставки в днях (разница между OrderDate и ShippedDate). 
+В результатах должны быть возвращены заказы, срок которых превышает переданное значение или еще недоставленные заказы. 
+Значению по умолчанию для передаваемого срока 35 дней. 
+Название процедуры ShippedOrdersDiff. 
+Процедура должна высвечивать следующие колонки: 
+OrderID, OrderDate, ShippedDate, ShippedDelay (разность в днях между ShippedDate и OrderDate), 
+SpecifiedDelay (переданное в процедуру значение).
+*/
+GO
+CREATE OR ALTER PROCEDURE ShippedOrdersDiff (@SpecifiedDelay int)
+AS
+BEGIN
+IF(@SpecifiedDelay is null) SET @SpecifiedDelay = 35
+SELECT OrderID, OrderDate, ShippedDate, 
+	DAY(ShippedDate - OrderDate) as SDelay, @SpecifiedDelay as [SpecifiedDelay]
+FROM Orders
+WHERE DAY(ShippedDate - OrderDate) > @SpecifiedDelay OR DAY(ShippedDate - OrderDate) = NULL;
+END;
+GO
+
+
+/*13.3
+Написать процедуру, которая высвечивает всех подчиненных заданного продавца, 
+как непосредственных, так и подчиненных его подчиненных. 
+В качестве входного параметра функции используется EmployeeID. 
+Необходимо распечатать имена подчиненных 
+и выровнять их в тексте (использовать оператор PRINT) согласно иерархии подчинения. 
+Продавец, для которого надо найти подчиненных также должен быть высвечен. 
+Название процедуры SubordinationInfo. 
+В качестве алгоритма для решения этой задачи надо использовать пример, 
+приведенный в Books Online и рекомендованный Microsoft для решения подобного типа задач. 
+Продемонстрировать использование процедуры.
+*/
+GO
+CREATE OR ALTER PROCEDURE SubordinationInfo (@empID int)
+AS
+BEGIN
+SELECT CONCAT(Emp.FirstName, ' ', Emp.LastName)
+FROM Employees as Emp
+WHERE Emp.ReportsTo IN
+	(SELECT E.EmployeeID
+	FROM Employees as E
+	WHERE E.ReportsTo = @empID) OR
+	Emp.ReportsTo = @empID
+END;
+GO
+
+/*13.4
+Написать функцию, которая определяет, есть ли у продавца подчиненные. 
+Возвращает тип данных BIT. 
+В качестве входного параметра функции используется EmployeeID. 
+Название функции IsBoss. 
+Продемонстрировать использование функции для всех продавцов из таблицы Employees.
+*/
+CREATE OR ALTER FUNCTION dbo.IsBoss(@empID AS int)
+RETURNS BIT
+AS
+BEGIN
+RETURN
+(SELECT CASE WHEN EXISTS
+	(SELECT *
+	FROM Employees as E
+	WHERE E.ReportsTo = @empID)
+	THEN 1
+	ELSE 0
+	END)
+END;
+GO
